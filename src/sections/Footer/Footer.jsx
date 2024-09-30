b5ffb840a4b559d2c8da9f80e723f79ae92e0a29
Rohith R <84264403+Rohith-chrkd@users.noreply.github.com>
2024-09-30 23:55:17 +0530

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Sai Kishor. <br />
+        &copy; 2024 Rohith. <br />
         All rights reserved.
       </p>
     </section>
